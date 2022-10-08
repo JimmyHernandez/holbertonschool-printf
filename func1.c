5f125b242bcd1b7b7f5332bf802321afd7f4f7bf
aryamalvrz <aryamalvrz@gmail.com>
2022-10-08 17:51:25 +0000

    Adding 2 extra functions

--- func1.c
+++ func1.c
@@ -9,17 +9,17 @@
  */
 int print_char(va_list ptr)
 {
-    char c;
-    c = va_arg(ptr,int);
+	char c;
+	c = va_arg(ptr,int);
 
-    if (c == '\0')
-       {
-            return ('\0');
-       }
-      
-       _putchar(c);
+	if (c == '\0')
+	{
+		return ('\0');
+	}
 
-       return (c);
+	_putchar(c);
+
+	return (c);
 }
 /**
  * print_string - prints a sequence of character.
@@ -28,20 +28,67 @@ int print_char(va_list ptr)
  */
 int print_string(va_list ptr)
 {
-    char *str;
-    int i;
+	char *str;
+	int i;
 
-    str = va_arg(ptr, char *);
+	str = va_arg(ptr, char *);
 
-    if (str == '\0')
-        {
-            _putchar('\0');
-        }
+	if (str == '\0')
+	{
+		_putchar('\0');
+	}
 
-    for (i = 0 ; str[i] != '\0';i++)
-        {
-            _putchar(str[i]);
-        }
+	for (i = 0 ; str[i] != '\0';i++)
+	{
+		_putchar(str[i]);
+	}
 
-    return (str);
+	return (str);
 }
+
+/**
+ * print_numbers - Prints the integer
+ * @arp: Argument Pointer
+ *
+ * Return: Count of all numbers printed
+ */
+
+int print_numbers(va_list arp)
+{
+	int num = 0, count = 0;
+
+	num = va_arg(arp, int);
+	count += print_int(num);
+
+	return(count);
+}
+
+/**
+ * print_int - Prints the integer
+ * @i: Number to be printed
+ *
+ * Return: Count of all numbers printed
+ */
+
+int print_int(int i)
+{
+	int count = 1, unsigned int num = 0;
+
+	if (i < 0)
+	{
+		_putchar('-');
+		count++;
+		num = i * -1;
+	}
+	else
+	{
+		num i;
+	}
+	if (num/10)
+	{
+		count += print_int(num/10);
+	}
+	_putchar((num % 10) + 48;
+
+			return(count);
+			}
