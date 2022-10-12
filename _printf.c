7f0b50d42c8e0c4faf9905c999cac75b0bb38db9
aryamalvrz <aryamalvrz@gmail.com>
2022-10-12 16:40:39 +0000

    Fixed syntax and betty comments

--- _printf.c
+++ _printf.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _printf - A Function that prints in stdout
- *@format:the parameters passed into the function.
- *Return: character to be printed.
+ *@format: The parameters passed into the function.
+ *Return: Character to be printed.
  */
 int _printf(const char *format, ...)
 {
@@ -10,49 +10,41 @@ int _printf(const char *format, ...)
 	int idx, idx2;
 	int count = 0;
 
-		 pick_t option[] = {
+	pick_t option[] = {
 		{'c', print_char},
 		{'s', print_string},
 		{'%', print_percentage},
 		{'d', print_numbers},
 		{'i', print_numbers},
 		{'\0', NULL}};
-
-		va_start(ptr, format);
-
+	va_start(ptr, format);
 	if (ptr == NULL || format == NULL)
-		{return (-1); }
-
+		return (-1);
 	for (idx = 0; format[idx] != '\0'; idx++)
 	{
 		if (format[idx] != '%')
-		{	_putchar(format[idx]);
+		{
+			_putchar(format[idx]);
 			count++;
 		}
 		if (format[idx] == '%')
 		{
 			if (format[idx + 1] == '%')
-			{	_putchar('%');
-				idx++;
-				count++;
-			}
-
 			{
-			if (format[idx + 1] == '%')
-			{	_putchar('%');
+				_putchar('%');
 				idx++;
 				count++;
 			}
-
 			for (idx2 = 0; idx2 < 4; idx2++)
 			{
 				if (format[idx + 1] == option[idx2].data)
-				{	count += option[idx2].f(ptr);
+				{
+					count += option[idx2].f(ptr);
 					idx++;
 				}
 			}
 		}
 	}
- } va_end(ptr);
+} va_end(ptr);
 return (count);
 }
